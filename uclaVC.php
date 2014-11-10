fcd76224eca506cdcf24ee5a7b2302c74524193a
dewal gupta <dewalgupta@gmail.com>
2014-11-10 03:07:03 -0800

    added dependency injection for DB connection handling

--- uclaVC.php
+++ uclaVC.php
@@ -1,61 +1,45 @@
 <?php 
 /*
 TO DO: 
-3. ADD GLOBAL HANDLER FOR DB CONNECTION 
-4. CLEAN UP CODE
-5. ADD VIEWING/QUERYING ABILITY 
+1. ADD VIEWING/QUERYING ABILITY 
 */
 
 require_once("permissions.php");
 
-$raw_data = file_get_contents($url);
-
-$data = json_decode($raw_data, true);
-
-//var_dump($data);
-
-
-//CONNECTING TO THE DATABASE
-
-
-try { //try connecting to $dbname at localhost
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "Connected to $dbname at $host successfully. <br>";
-    
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-    
-    $db = null;
-
-} catch (PDOException $pe) { //if it couldn't connect
-   
-   	try { //...is it a connection problem? 
-    	$db = new PDO("mysql:host=$host", $username, $password);
-    } catch (PDOException $pe) {
-    	die("DB ERROR: " . $pe->getMessage());
-    } 
-    
-    $db = new PDO("mysql:host=$host", $username, $password);
-    //if not connection, then db doesn't exist, so set it up:
-    $db->query("CREATE DATABASE IF NOT EXISTS `$dbname`");
-	$db->query("USE `$dbname`");
+class container extends permissions {
 	
-	$table_setup = "CREATE TABLE IF NOT EXISTS `ucla`(
-  		`id` int(10) NOT NULL,
-  		`pic` varchar(150) NULL,
-  		`url` varchar(150) NULL,
-  		`name` varchar(150)  NULL,
-  		`type` varchar(20)  NULL
-	)";
-	
-	$db->exec($table_setup);
-		
-	print "created new database $dbname! <br> ";
-} 
+	public static function newDBconn() {
 
-//adding data to the database
+		try { //try connecting to $dbname at localhost
+   			$db = new PDO(parent::$conn, parent::$username, parent::$password);
+    		echo "Connected to ". parent::$dbname ." at ". parent::$host ." successfully. <br>";
+    
+    		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+    		
+			} catch (PDOException $pe) { //if it couldn't connect
+				die("DB ERROR: " . $pe->getMessage());
+			}
+			
+		return $db;	
+	}
+}
+
+function getData($url) {
+	$raw_data = file_get_contents($url);
+	$parsed_data = json_decode($raw_data, true);
+	
+	return $parsed_data;
+}
+
+
+$url = "https://api.angel.co/1/search?query=ucla&type=User";
+$data = getData($url);
+
+
+$db = container::newDBconn();
 
 try {
-	$db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+	//$db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 	$sql = $db->prepare("INSERT INTO `ucla` (`id`, `pic`, `url`, `name`, `type`) value (:id, :pic, :url, :name, :type)");
 	foreach ($data as $user) {
 		$sql->execute($user);
@@ -67,6 +51,6 @@ try {
 	die("DB ERROR: " . $pe->getMessage());
 }
 
- 
-    
- ?>
+$db = null;
+
+?>
