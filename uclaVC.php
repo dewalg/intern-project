73acd41af60ef6812afc5a04e0a73413929b2b29
dewal gupta <dewalgupta@gmail.com>
2014-11-15 19:38:04 -0800

    finished all viewing functions

--- uclaVC.php
+++ uclaVC.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<style>
 	table.db-table 		{ border-right:1px solid #ccc; border-bottom:1px solid #ccc; }
-	table.db-table th	{ background:#eee; padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
-	table.db-table td	{ padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+	table.db-table th	{ background:#eee; padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; font-family: 'Open Sans', serif; font-size:15px; }
+	table.db-table td	{ padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; font-family: 'Open Sans', serif; font-size:10px; }
 	</style>
 </head>
 
 <body>
 <?php
-
-/* TO DO:
-1. ADD LOCATION, ROLES AND SKILLS TO VIEWING
-*/
 
 require_once("permissions.php");
 
@@ -23,7 +20,6 @@ class container extends permissions {
 
 		try { //try connecting to $dbname at localhost
    			$db = new PDO(parent::$conn, parent::$username, parent::$password);
-    		echo "Connected to ". parent::$dbname ." at ". parent::$host ." successfully. <br>";
     
     		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     		
@@ -40,6 +36,70 @@ function getData($url) {
 	$parsed_data = json_decode($raw_data, true);
 	
 	return $parsed_data;
+}
+
+function prepareURL() {
+	
+	if ($user['facebook_url']) {
+		$facebook = "<a href=\"".$user['facebook_url']."\">
+		<img src=\"http://img2.wikia.nocookie.net/__cb20130501121248/logopedia/images/f/fb/Facebook_icon_2013.svg\" height=\"25\" width=\"25\"></a>";
+	} else {
+		$facebook = false;
+	}
+	
+	if ($user['twitter_url']) {
+		$twitter = "<a href=\"".$user['twitter_url']."\">
+		<img src=\"https://cdn1.iconfinder.com/data/icons/simple-icons/4096/twitter-4096-black.png\" height=\"25\" width=\"25\"></a>";
+	} else {
+		$twitter = false;
+	}
+	
+	if ($user['github_url']) {	
+		$github = "<a href=\"".$user['github_url']."\">
+		<img src=\"http://fc05.deviantart.net/fs71/i/2012/223/4/3/github_flurry_ios_style_icon_by_flakshack-d5ariic.png\" height=\"25\" width=\"25\"></a>";
+	} else {
+		$github = false;
+	}
+	
+	if ($user['linkedin_url']) {	
+		$linkedin = "<a href=\"".$user['linkedin_url']."\">
+		<img src=\"https://cdn1.iconfinder.com/data/icons/simple-icons/4096/linkedin-4096-black.png\" height=\"25\" width=\"25\"></a>";
+	} else {
+		$linkedin = false;
+	}
+	
+	if ($user['dribbble_url']) {		
+		$dribbble = "<a href=\"".$user['dribbble_url']."\">
+		<img src=\"https://cdn3.iconfinder.com/data/icons/unicons-vector-icons-pack/32/dribbble-512.png\" height=\"25\" width=\"25\"></a>";
+	} else {
+		$dribbble = false;
+	}	
+	
+	if ($user['behance_url']) {	
+		$behance = "<a href=\"".$user['behance_url']."\">
+		<img src=\"https://cdn3.iconfinder.com/data/icons/picons-social/57/77-behance-512.png\" height=\"25\" width=\"25\"></a>";
+	} else {
+		$behance = false;
+	}			
+
+	$URLs = null;
+	if ($facebook) {
+		$URLs .= $facebook;
+	} else if ($twitter) {
+		$URLs .= $twitter;
+	} else if ($github) {
+		$URLs .= $github;
+	} else if ($linkedin) {
+		$URLs .= $linkedin;
+	} else if ($dribbble) {
+		$URLs .= $dribbble;
+	} else if ($behance) {
+		$URLs .= $behance;
+	} 
+
+
+	return $URLs;
+	
 }
 
 $db = container::newDBconn();
@@ -175,7 +235,7 @@ try {
 	
 	echo '<table cellpadding="0" cellspacing="0" class="db-table">';
 	echo '<tr><th>Person</th><th>bio</th><th>What I\'ve Built</th><th>What I Do</th>
-		<th>Criteria</th><th>follower count</th></tr>';
+		<th>Criteria</th><th>Locations</th><th>Roles</th><th>Skills</th></tr>';
 	
 	foreach ($query->FetchAll() as $user) {
 		if ($user['pic'] == null) {
@@ -188,40 +248,156 @@ try {
 			$user['investor'] = "";
 		}
 		
-		$facebook_link = "<a href=\"".$user['facebook_url']."\">
-		<img src=\"http://img2.wikia.nocookie.net/__cb20130501121248/logopedia/images/f/fb/Facebook_icon_2013.svg\" height=\"25\" width=\"25\"></a>";
+		///////////////////////////////////
 		
-		$twitter_link = "<a href=\"".$user['twitter_url']."\">
-		<img src=\"https://cdn1.iconfinder.com/data/icons/simple-icons/4096/twitter-4096-black.png\" height=\"25\" width=\"25\"></a>";
-		
-		$github_link = "<a href=\"".$user['github_url']."\">
-		<img src=\"http://fc05.deviantart.net/fs71/i/2012/223/4/3/github_flurry_ios_style_icon_by_flakshack-d5ariic.png\" height=\"25\" width=\"25\"></a>";
-		
-		$linkedin_link = "<a href=\"".$user['linkedin_url']."\">
-		<img src=\"https://cdn1.iconfinder.com/data/icons/simple-icons/4096/linkedin-4096-black.png\" height=\"25\" width=\"25\"></a>";
-		
-		$dribbble_link = "<a href=\"".$user['dribbble_url']."\">
-		<img src=\"https://cdn3.iconfinder.com/data/icons/unicons-vector-icons-pack/32/dribbble-512.png\" height=\"25\" width=\"25\"></a>";
-		
-		$behance_link = "<a href=\"".$user['behance_url']."\">
-		<img src=\"https://cdn3.iconfinder.com/data/icons/picons-social/57/77-behance-512.png\" height=\"25\" width=\"25\"></a>";
-					
-		$onlinebio_link = "<br><a href=\"".$user['online_bio_url']."\">Online Bio</a><br>";
-		$aboutme_link = "<a href=\"".$user['aboutme_url']."\">About me</a><br>";
-		$blog_link = "<a href=\"".$user['blog_url']."\">Blog</a><br>";
-		$resume_link = "<a href=\"".$user['resume_url']."\">Resume</a>";
+		if ($user['facebook_url']) {
+			$facebook = "<a href=\"".$user['facebook_url']."\">
+			<img src=\"http://img2.wikia.nocookie.net/__cb20130501121248/logopedia/images/f/fb/Facebook_icon_2013.svg\" height=\"25\" width=\"25\"></a>";
+		} else {
+			$facebook = false;
+		}
+	
+		if ($user['twitter_url']) {
+			$twitter = "<a href=\"".$user['twitter_url']."\">
+			<img src=\"https://cdn1.iconfinder.com/data/icons/simple-icons/4096/twitter-4096-black.png\" height=\"25\" width=\"25\"></a>";
+		} else {
+			$twitter = false;
+		}
+	
+		if ($user['github_url']) {	
+			$github = "<a href=\"".$user['github_url']."\">
+			<img src=\"http://fc05.deviantart.net/fs71/i/2012/223/4/3/github_flurry_ios_style_icon_by_flakshack-d5ariic.png\" height=\"25\" width=\"25\"></a>";
+		} else {
+			$github = false;
+		}
+	
+		if ($user['linkedin_url']) {	
+			$linkedin = "<a href=\"".$user['linkedin_url']."\">
+			<img src=\"https://cdn1.iconfinder.com/data/icons/simple-icons/4096/linkedin-4096-black.png\" height=\"25\" width=\"25\"></a>";
+		} else {
+			$linkedin = false;
+		}
+	
+		if ($user['dribbble_url']) {		
+			$dribbble = "<a href=\"".$user['dribbble_url']."\">
+			<img src=\"https://cdn3.iconfinder.com/data/icons/unicons-vector-icons-pack/32/dribbble-512.png\" height=\"25\" width=\"25\"></a>";
+		} else {
+			$dribbble = false;
+		}	
+	
+		if ($user['behance_url']) {	
+			$behance = "<a href=\"".$user['behance_url']."\">
+			<img src=\"https://cdn3.iconfinder.com/data/icons/picons-social/57/77-behance-512.png\" height=\"25\" width=\"25\"></a>";
+		} else {
+			$behance = false;
+		}			
 
-		$URLs = $facebook_link.$twitter_link.$github_link.$linkedin_link.$dribbble_link.$behance_link 
-				."<br>".$onlinebio_link." | ".$aboutme_link." | ".$blog_link." | ".$resume_link;
+		$URLs = null;
+		if ($facebook) {
+			$URLs .= $facebook;
+		} 
+		if ($twitter) {
+			$URLs .= $twitter;
+		} 
+		 if ($github) {
+			$URLs .= $github;
+		} 
+		 if ($linkedin) {
+			$URLs .= $linkedin;
+		} 
+		 if ($dribbble) {
+			$URLs .= $dribbble;
+		} 
+		 if ($behance) {
+			$URLs .= $behance;
+		} 
+		
+		
+		
+		if ($user['online_bio_url']) {	
+			$onlinebio = "<a href=\"".$user['online_bio_url']."\">Online Bio</a><br>";
+		} else {
+			$onlinebio = false;
+		}	
+			
+		if ($user['aboutme_url']) {	
+			$aboutme = "<a href=\"".$user['aboutme_url']."\">About Me</a><br>";
+		} else {
+			$aboutme = false;
+		}	
+		
+		if ($user['blog_url']) {	
+			$blog = "<a href=\"".$user['blog_url']."\">Blog</a><br>";
+		} else {
+			$blog = false;
+		}	
+		
+		if ($user['resume_url']) {	
+			$resume = "<a href=\"".$user['resume_url']."\">Resume</a><br>";
+		} else {
+			$resume = false;
+		}	
+			
+
+		$URLs2 = null;
+		if ($onlinebio) {
+			$URLs2 .= $onlinebio;
+		}
+		if ($aboutme) {
+			$URLs2 .= $aboutme;
+		}
+		if ($blog) {
+			$URLs2 .= $blog;
+		}
+		if ($resume) {
+			$URLs2 .= $resume;
+		}
+		
+		
+		///////////////////////////////////
+					
+		//GET LOCATIONS ASSOCIATED WITH USER		
+		
+		$query = $db->prepare("SELECT * FROM `locations` WHERE user_id = :id");
+		$query->bindParam(':id', $user['id']);
+		$query->execute();
+		
+		$location_of_person = null;
+		foreach ($query->fetchAll() as $loc) {
+			$location_of_person .= $loc['display_name']."<br>";
+		}
+		
+		//GET SKILL ASSOCIATED WITH USER		
+		
+		$query = $db->prepare("SELECT * FROM `skills` WHERE user_id = :id");
+		$query->bindParam(':id', $user['id']);
+		$query->execute();
+		
+		$skills_of_person = null;
+		foreach ($query->fetchAll() as $sk) {
+			$skills_of_person .= $sk['display_name']."<br>";
+		}
+		
+		//GET ROLES ASSOCIATED WITH USER		
+		
+		$query = $db->prepare("SELECT * FROM `roles` WHERE user_id = :id");
+		$query->bindParam(':id', $user['id']);
+		$query->execute();
+		
+		$roles_of_person = null;
+		foreach ($query->fetchAll() as $ro) {
+			$roles_of_person .= $ro['display_name']."<br>";
+		}
 		
 		print "<tr><td><a href=\"".$user['angellist_url']."\">".$user['name']."</a> ".$user['investor']."<p>
-				<img src=\"".$user['image']."\"><br>User ID: ".$user['id']."<p>".$URLs."</td>
-				<td>".$user['bio']."</td>
+				<img src=\"".$user['image']."\"><br>User ID: ".$user['id']."<p>".$URLs."<p> Followers: ".$user['follower_count']."</td>
+				<td>".$user['bio']."<p>".$URLs2."</td>
 				<td>".$user['what_ive_built']."</td>
 				<td>".$user['what_i_do']."</td>
 				<td>".$user['criteria']."</td>
-				<td>".$user['follower_count']."</td>
-				<td></td>
+				<td>".$location_of_person."</td>
+				<td>".$roles_of_person."</td>
+				<td>".$skills_of_person."</td>
 				</tr>";
 	}
 	
